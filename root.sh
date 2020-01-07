ff1ff38cd817e7ec6a1eb88b411d59fa97d7c487
amine10a <50719983+amine10a@users.noreply.github.com>
2020-01-07 13:08:43 +0100

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,16 @@
+clear
+pkg install tsu
+pkg install figlet
+clear
+figlet PhoneRoot
+echo                                   mr :amine10a
+echo
+echo
+cp  KingRoot.apk /sdcard
+echo ___________________________
+echo                                                   
+echo       go to sdcard and 
+echo    install KingRoot now
+echo __________________________
+termux-open KingRoot.apk
+
